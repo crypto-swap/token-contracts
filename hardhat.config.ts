4451711592d5feefa9a29df037931e0893858541
natlou <natloudev@gmail.com>
2022-07-26 17:29:02 -0700

    removed deprecated optimizer setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+module.exports = {
+  zksolc: {
+    version: "0.1.0",
+    compilerSource: "docker",
+    settings: {
+      experimental: {
+        dockerImage: "matterlabs/zksolc",
+      },
+    },
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
+    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+  },
+  networks: {
+    // To compile with zksolc, this must be the default network.
+    hardhat: {
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.12",
+  },
 };
-
-export default config;
